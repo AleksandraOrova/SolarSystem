3f6e7a2d92d7b11dd830d4c1382bb5273cc551c0
Anton Lamtev <antonlamtev@gmail.com>
2016-05-03 18:41:24 +0300

    review

--- sources2/Solar_S/Core/timehoursseconds.h
+++ sources2/Solar_S/Core/timehoursseconds.h
@@ -1,15 +1,23 @@
 #ifndef TIMEHOURSSECONDS_H
 #define TIMEHOURSSECONDS_H
 
-
+//TODO задокументировать Doxygen'ом
 class TimeHoursSeconds
 {
 private:
+//Пышкин на лекциях говорил, что классно сначала объявлять public функции (интерфейс класса, функции доступа к данным)
+//А после них уже private функции и данные.
+//Это можно аргументировать тем, что удобно видеть пользователю класса сначала то, что можно будет ему использовать, а
+//не то, что закрыто.
+//TODO поменять местами public и private.
     double second;
     double hour;
     double day;
     double year;
 public:
+//чтобы понять, что такое s, h, d и y, надо смотреть на данные класса. Посмотрев только на интерфейс, что будет делать пользователь,
+//понять ничего нельзя.
+//TODO дать нормальные названия переменным в констукторе
     TimeHoursSeconds(double s, double h, double d, double y);
     double fromStoHours();
     double fromStoDays();
