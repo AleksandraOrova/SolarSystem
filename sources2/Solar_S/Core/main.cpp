fa6a9fb32fffd023edb0fa1eb7903e55438b6691
Aleksandra <alexorova310198@icloud.com>
2016-05-11 23:57:10 +0300

    Changes after review

--- sources2/Solar_S/Core/main.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-//TODO удалить main из библиотеки
-// todo исправить предыдущее todo. И не надо писать строчку ниже
-using namespace std;
-
-int main(){
-    cout << "Hello, Banana!" << endl;
-    return 0;
-}
